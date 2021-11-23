03110377265c1048e83981ebb2532e3f3375d04b
devhulk <gry2big93@gmail.com>
2021-11-23 18:05:06 -0500

    updated module version

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "read_endpoints" {
     value = module.cosmosdb.read_endpoints
+    sensitive = true
 }
 
 output "write_endpoints" {
     value = module.cosmosdb.write_endpoints
+    sensitive = true
 }
 
 output "connection_strings" {
     value = module.cosmosdb.connection_strings
+    sensitive = true
 }
